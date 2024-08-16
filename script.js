299a1bc7b66bb0a591adca9beef42ba222966c16
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-08-16 13:27:09 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const name = "Ruslan and Ali";
+
+// console.log(name.length); // 4 (metnin sayir)
+//---------------------------------------------------
+
+// console.log(name.includes("and")); //true
+// console.log(name.includes("and", 7)); //true (baslangicdan and-e qeder true onan sonra false olacax!)
+// console.log(name.includes("and", 8)); //false (baslangicdan and-e qeder true onan sonra false olacax!)
+// console.log(name.includes("d ")); //true
+// console.log(name.includes("8nd")); //false
+// eyer metndeki yaziraldan biri varsa true verecek. Eyer yoxdusa false qaytaracax!
+
+//---------------------------------------------------
