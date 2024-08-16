00449f5d4312a38a924657e31a819acf15bb6e3f
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-08-16 13:39:46 +0400

    method indexOf

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const name = "Ruslan and Ali";
 
-// console.log(name.length); // 4 (metnin sayir)
+// console.log(name.length); // 14 (metnin sayir)
 //---------------------------------------------------
 
-// console.log(name.includes("and")); //true
+// console.log(name.includes("us")); //true
 // console.log(name.includes("and", 7)); //true (baslangicdan and-e qeder true onan sonra false olacax!)
 // console.log(name.includes("and", 8)); //false (baslangicdan and-e qeder true onan sonra false olacax!)
 // console.log(name.includes("d ")); //true
@@ -11,3 +11,7 @@ const name = "Ruslan and Ali";
 // eyer metndeki yaziraldan biri varsa true verecek. Eyer yoxdusa false qaytaracax!
 
 //----------------------------------------------------------------------------------------------
+
+// console.log(name.indexOf("and Ali")); // 7 necenciden basladigini gosterir (eyer sef olarsa -1 verecek)
+
+// console.log(name.indexOf("Ali", 11)); // 11 reqemi 0-dan 11-e qeder ne reqem yazsam 11 verecek (eyer sef yazsam -1 verecek)
